ee91e48d97c49ef0405a86451b30d2721f84ed9e
Shianne <ShianneJT@gmail.com>
2020-05-01 07:38:33 -0600

    Initial commit

---
+++ liri.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+
+// Make it so liri.js can take in one of the following commands:
+
+
+// concert-this
+
+
+// spotify-this-song
+
+
+// movie-this
+
+
+// do-what-it-say
+
