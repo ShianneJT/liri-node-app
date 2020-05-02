7ceaa6aa9e5aa9610b81215dced96057527d6904
Shianne <ShianneJT@gmail.com>
2020-05-01 21:34:06 -0600

    movie this working individually

--- liri.js
+++ liri.js
@@ -7,10 +7,48 @@ var moment = require('moment');
 // var spotify = new Spotify(keys.spotify);
 
 
-// Bands in Town
+// concert-this
+
+var category = process.argv[2];
+var choiceRaw = process.argv.splice(3, process.argv.length-1);
+var choice = choiceRaw.join(' ');
+
+var queryURL = 'https://rest.bandsintown.com/artists/' + choice + '/events?app_id=codingbootcamp'
+
+if (category === 'concert-this') {
+    axios.get(queryURL).then(
+        function(response) {
+            var concertDate = response.data[0].datetime;
+ 
+            var convertedDate = moment(concertDate).format('MM/DD/YYYY');
+            console.log('-----' + response.data[0].artist.name + '-----')
+            console.log('Name of venue: ' + response.data[0].venue.name);
+            console.log('Venue Location: ' + response.data[0].venue.location);
+            console.log('Event Date: ' + convertedDate);
+        }
+        )
+}
+
+
+
+
+
+
+
+/* 
+
 var category = process.argv[2]
 var choice = process.argv[3];
 console.log(category, choice); 
+
+
+
+
+
+
+
+
+
 var queryURL = 'https://rest.bandsintown.com/artists/' + choice + '/events?app_id=codingbootcamp'
 
 if (category === 'concert-this') {
@@ -26,6 +64,52 @@ if (category === 'concert-this') {
         )
 }
 
+*/
+
+// movie-this
+
+/*
+
+var category = process.argv[2]
+var choice = process.argv[3];
+console.log(category, choice); 
+
+
+choice=choice.replace(/s/g,"%20");
+
+
+var queryURL = 'https://www.omdbapi.com/?t=' + choice + '&apikey=trilogy';
+
+if (category === 'movie-this') {
+    axios.get(queryURL).then(
+        function(response) {
+            var movieInfo = response.data
+            console.log('Title: ' + movieInfo.title);
+            console.log('IMDB Rating: ' + movieInfo.ratings[0].value);
+            console.log('Rotten Tomatoes Rating: ' + movieInfo.ratings[1].value);
+            console.log('County of Origin: ' + movieInfo.country);
+            console.log('Language: ' + movieInfo.language);
+            console.log('Plot: ' + movieInfo.plot);
+            console.log('Actors: ' + movieInfo.actors);
+        }
+    )
+}
+
+// * Title of the movie.
+
+// * Year the movie came out.
+// * IMDB Rating of the movie.
+// * Rotten Tomatoes Rating of the movie.
+// * Country where the movie was produced.
+// * Language of the movie.
+// * Plot of the movie.
+// * Actors in the movie.
+
+
+
+
+
+
 // Make it so liri.js can take in one of the following commands:
 // 
 // concert-this
@@ -36,9 +120,5 @@ if (category === 'concert-this') {
 // 
 // do-what-it-says
 
-// node liri.js concert-this <artist/band name here>
-// This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
-// Name of the venue
-// Venue location
-// Date of the Event (use moment to format this as "MM/DD/YYYY")
 
+*/
