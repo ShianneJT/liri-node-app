9b3e93fff009adfc889a4ccb4a564253d5f195d1
Shianne <ShianneJT@gmail.com>
2020-05-03 20:51:58 -0600

    add default results to search

--- README.md
+++ README.md
@@ -6,28 +6,33 @@
 
 **LIRI Bot** can be used to search for upcoming **concerts** for your favorite artist/band, as well as information on **songs** and **movies**.
 
-### Setup
+![LIRI Bot Interface](https://shiannejt.github.io/liri-node-app/images/01.PNG)
+
+## Setup
 
 - **Node.js** is required
-  
+
 1. Clone the **liri-node-app** to your computer.
 2. Run the following command prompt in **LIRI Bot's** root folder:  
 ``` npm install ```
 
-### Using LIRI Bot
-
 To begin, simply type ``` node liri ``` in the command prompt
 
-  **LIRI Bot** accepts the following commands:
+## Commands
+
+**LIRI Bot** accepts the following commands:
 
 - ``` concert-this <artist or band name> ```
 
+![LIRI Bot Interface](https://shiannejt.github.io/liri-node-app/images/02.PNG)
+
 - ``` spotify-this-song <song title> ```
+
+![LIRI Bot Interface](https://shiannejt.github.io/liri-node-app/images/03.PNG)
 
 - ``` movie-this <movie title> ```
 
-- ``` do-what-it-says ```
+![LIRI Bot Interface](https://shiannejt.github.io/liri-node-app/images/04.PNG)
 
-#### Examples
-
-WIP
+- ``` do-what-it-says ```  
+Running this command uses the Node.js File System Module to read and pull information on the content inside the random.txt file, e.g. *spotify-this-song,"song title"*.
--- liri.js
+++ liri.js
@@ -3,17 +3,15 @@ require('dotenv').config();
 var fs = require('fs');
 var axios = require('axios');
 var moment = require('moment');
-var Spotify = require('node-spotify-api');
 var keys = require('./keys');
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-
 var bandsID = keys.bandsID.id;
 var omdbKey = keys.omdbKey.key;
-
 var category = process.argv[2];
 var choice = process.argv.slice(3).join(" ");
 
-switch(category) {
+switch (category) {
     case 'concert-this':
         logo();
         concertThis();
@@ -24,120 +22,117 @@ switch(category) {
         break;
     case 'movie-this':
         logo();
-        movieThis()
+        movieThis();
         break;
     case 'do-what-it-says':
         logo();
         doThis();
         break;
     default:
-        fs.readFile('ascii/intro.txt', 'utf8', function(err,data){
+        fs.readFile('ascii/intro.txt', 'utf8', function (err, data) {
             if (err) {
                 console.log(err);
-            } else {
-                console.log(data);
             }
+            else {
+                console.log(data);
+            };
         });
-}
+};
 
 // concert-this
 function concertThis() {
     var queryURL = 'https://rest.bandsintown.com/artists/' + choice + '/events?app_id=' + bandsID;
     axios.get(queryURL)
-        .then(function(response) {
-            var results = response.data[0]
-            var concertDate = results.datetime; 
+        .then(function (response) {
+            var results = response.data[0];
+            var concertDate = results.datetime;
             var convertedDate = moment(concertDate).format('MM/DD/YYYY');
 
-            console.log('\n    ---------------------------------------------------\n')
+            console.log('\n    ---------------------------------------------------\n');
             console.log('     Artist:         ' + results.artist.name);
             console.log('     Name of venue:  ' + response.data[0].venue.name);
             console.log('     Venue Location: ' + response.data[0].venue.location);
             console.log('     Event Date:     ' + convertedDate);
-            console.log('\n    ---------------------------------------------------')
+            console.log('\n    ---------------------------------------------------');
             console.log("\n   Type 'node liri' to return to the menu");
-    })
-    .catch(error => {
-        console.log('    ' + error);
-    });
+        })
+        .catch(error => {
+            console.log('ERRRRRROR' + error);
+        });
 };
 
 // spotify-this-song
-function spotifyThis() {
+function spotifyThis(choice) {
+    if (choice === undefined) {
+        choice = 'The Sign';
+    };
     spotify
-        .search({ type: 'track', query: choice, limit: 1 },
-        function(err, data) {
-            if (err) {
-                console.log('Error: ' + err);
-                return
-            } else {
-                console.log('\n     -----------------------------------------------\n')
-                console.log('      Artist:        ' + data.tracks.items[0].artists[0].name);
-                console.log('      Song Title:    ' + data.tracks.items[0].name);
-                console.log('      Album Name:    ' + data.tracks.items[0].album.name);
-                console.log('      Preview Link:  ' + data.tracks.items[0].preview_url);
-                console.log('\n     -----------------------------------------------')
-                console.log("\n   Type 'node liri' to return to the menu");
-            }
-        })
+        .search({
+                type: 'track',
+                query: choice,
+                limit: 1
+            },
+            function (err, data) {
+                if (err) {
+                    console.log('Error: ' + err);
+                    return
+                }
+                else {
+                    console.log('\n     -----------------------------------------------\n')
+                    console.log('      Artist:        ' + data.tracks.items[0].artists[0].name);
+                    console.log('      Song Title:    ' + data.tracks.items[0].name);
+                    console.log('      Album Name:    ' + data.tracks.items[0].album.name);
+                    console.log('      Preview Link:  ' + data.tracks.items[0].preview_url);
+                    console.log('\n     -----------------------------------------------')
+                    console.log("\n   Type 'node liri' to return to the menu");
+                };
+            });
 };
 
 // movie-this
-function movieThis(){
+function movieThis() {
+    if (!choice) {
+        choice = 'Mr. Nobody';
+    };
     var queryURL = 'https://www.omdbapi.com/?t=' + choice + '&apikey=' + omdbKey;
     axios.get(queryURL)
-    .then(function(response) {
-        var movieInfo = response.data;
-        console.log('\n     -----------------------------------\n')
-        console.log('      Title: ' + movieInfo.Title); 
-        console.log('      Released: ' + movieInfo.Released);
-        console.log('      IMDB Rating: ' + movieInfo.Ratings[0].Value);
-        console.log('      Rotten Tomatoes Rating: ' + movieInfo.Ratings[1].Value);
-        console.log('      County of Origin: ' + movieInfo.Country);
-        console.log('      Language: ' + movieInfo.Language);
-        console.log('      Plot: ' + movieInfo.Plot);
-        console.log('      Actors: ' + movieInfo.Actors);
-        console.log('\n     -----------------------------------')
-        console.log("\n   Type 'node liri' to return to the menu");
-    });
+        .then(function (response) {
+            var movieInfo = response.data;
+            console.log('\n     -----------------------------------\n')
+            console.log('      Title: ' + movieInfo.Title);
+            console.log('      Released: ' + movieInfo.Released);
+            console.log('      IMDB Rating: ' + movieInfo.Ratings[0].Value);
+            console.log('      Rotten Tomatoes Rating: ' + movieInfo.Ratings[1].Value);
+            console.log('      County of Origin: ' + movieInfo.Country);
+            console.log('      Language: ' + movieInfo.Language);
+            console.log('      Plot: ' + movieInfo.Plot);
+            console.log('      Actors: ' + movieInfo.Actors);
+            console.log('\n     -----------------------------------')
+            console.log("\n   Type 'node liri' to return to the menu");
+        });
 };
 
 // do-what-it-says
 function doThis() {
-    fs.readFile('random.txt', 'utf8', function(err,data){
+    fs.readFile('random.txt', 'utf8', function (err, data) {
         if (err) {
             console.log(err);
-        } else {
+        }
+        else {
             random = data.split(',');
             choice = random[1];
-           spotifyThis(choice)
-        }
+            spotifyThis(choice)
+        };
     });
 };
 
-function logo(){
-    fs.readFile('ascii/' + category + '.txt', 'utf8', function(err,data){
+function logo() {
+    fs.readFile('ascii/' + category + '.txt', 'utf8', function (err, data) {
         if (err) {
             console.log(err);
-        } else {
-            console.log(data)
         }
-    })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        else {
+            console.log(data)
+        };
+    });
+};
